80e90cdc9cab3919fa52bdf58a18f88797e169ed
Parry Choi <parrychoi1109@gmail.com>
2024-11-12 17:08:30 +0100

    Add matrix functions

--- src/include/internal/matrix.hpp
+++ src/include/internal/matrix.hpp
@@ -2,82 +2,262 @@
 #define MATRIX_HPP
 
 #include <cstddef>
+#include <cstdlib>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
 #include <vector>
 
 namespace fin_diff {
 
-template <typename _T>
-class MatrixBase {
-   public:
-    MatrixBase(size_t rows, size_t cols) : rows(rows), cols(cols) {}
-    virtual ~MatrixBase() = default;
+    template <typename _T>
+    class MatrixBase {
+       public:
+        MatrixBase(size_t rows, size_t cols) : rows(rows), cols(cols) {}
+        // virtual ~MatrixBase() = default;
 
-    size_t get_rows() const;
-    size_t get_cols() const;
+        size_t get_n_rows() const;
+        size_t get_n_cols() const;
 
-    virtual _T get(size_t i, size_t j) const = 0;
-    virtual void set(size_t i, size_t j, _T val) = 0;
-    virtual void print() const = 0;
+        
+        virtual _T get(size_t i, size_t j) const = 0;
+        virtual void set(size_t i, size_t j, _T val) = 0;
 
-   protected:
-    size_t rows, cols;
 
-    void validate(size_t i, size_t j) const {
-        // if (i >= get_rows() || j >= get_cols()) {
-        //     throw std::out_of_range("Index out of range");
-        // }
-        if (i >= get_rows() || j >= get_cols()) {
-            std::ostringstream oss;
-            oss << "Index out of range: i=" << i << ", j=" << j
-                << ", rows=" << get_rows() << ", cols=" << get_cols();
-            throw std::out_of_range(oss.str());
+
+        template <typename _T2>
+        MatrixBase<_T>& operator+=(MatrixBase<_T2> o) {
+            if (this->get_n_rows() != o.get_n_rows() || this->get_n_cols() != o.get_n_cols()) {
+                throw std::invalid_argument("Matrix dimensions do not match");
+            }
+
+            for (int i = 0; i < this->get_n_cols() * this->get_n_rows(); i++) {
+                this->set(i / this->get_n_cols(), i % this->get_n_cols(), this->get(i / this->get_n_cols(), i % this->get_n_cols()) + o.get(i / this->get_n_cols(), i % this->get_n_cols()));
+            }
+
+            return *this;
         }
-    }
-};
 
-template <typename _T>
-class Matrix : public MatrixBase<_T> {
-   public:
-    Matrix(size_t rows, size_t cols);
-    ~Matrix();
+        virtual void print() const = 0;
 
-    _T get(size_t i, size_t j) const override;
-    void set(size_t i, size_t j, _T val) override;
+       protected:
+        size_t rows, cols;
 
-    void print() const override;
+        void validate(size_t i, size_t j) const {
+            if (i < 0 || j < 0 || i >= get_n_rows() || j >= get_n_cols()) {
+                std::ostringstream oss;
+                oss << "Index out of range: i=" << i << ", j=" << j
+                    << ", rows=" << get_n_rows() << ", cols=" << get_n_cols();
+                throw std::out_of_range(oss.str());
+            }
+        }
+    };
 
-   private:
-    _T *data;
-};
+    template <typename _T>
+    class Matrix : public MatrixBase<_T> {
+       public:
+        Matrix();
+        Matrix(size_t rows, size_t cols);
+        Matrix(size_t rows, size_t cols, _T val);
+        Matrix(size_t rows, size_t cols, std::vector<_T> vals);
+        // ~Matrix();
 
-template <typename _T>
-class MatrixCRS : public MatrixBase<_T> {
-   public:
-    MatrixCRS(size_t n);
-    MatrixCRS(size_t rows, size_t cols);
-    ~MatrixCRS();
+        
+        _T get(size_t i, size_t j) const override;
+        void set(size_t i, size_t j, _T val) override;
 
-    void construct(size_t rows, size_t cols);
+        // Overload the operator() for two-dimensional access
+        _T& operator()(size_t row, size_t col);
+        const _T& operator()(size_t row, size_t col) const;
 
-    _T get(size_t i, size_t j) const override;
-    void set(size_t i, size_t j, _T val) override;
+        Matrix<_T>& operator+=(_T val) {
+            for (int i = 0; i < this->get_n_cols() * this->get_n_rows(); i++) {
+                data[i] += val;
+            }
 
-    static MatrixCRS<_T> from_dense(const Matrix<_T> &mat);
+            return *this;
+        }
 
-    void print() const override;
+        template <typename _T2>
+        Matrix<_T>& operator+=(Matrix<_T2> o) {
+            if (this->get_n_rows() != o.get_n_rows() || this->get_n_cols() != o.get_n_cols()) {
+                throw std::invalid_argument("Matrix dimensions do not match");
+            }
 
-   private:
-    std::vector<_T> values;
-    std::vector<size_t> col_indices;
-    size_t *row_ptrs;
+            for (int i = 0; i < this->get_n_cols() * this->get_n_rows(); i++) {
+                data[i] += o.data[i];
+            }
 
-    void _remove(size_t i, size_t j);
-    size_t _find(size_t i, size_t j) const;
-};
+            return *this;
+        }
 
+        Matrix<_T>& operator-=(_T val) {
+            for (int i = 0; i < this->get_n_cols() * this->get_n_rows(); i++) {
+                data[i] -= val;
+            }
+
+            return *this;
+        }
+
+        Matrix<_T>& operator*=(_T val) {
+            for (int i = 0; i < this->get_n_cols() * this->get_n_rows(); i++) {
+                data[i] *= val;
+            }
+
+            return *this;
+        }
+
+        Matrix<_T>& operator/=(_T val) {
+            for (int i = 0; i < this->get_n_cols() * this->get_n_rows(); i++) {
+                data[i] /= val;
+            }
+
+            return *this;
+        }
+
+        void clear() {
+            for (int i = 0; i < this->get_n_cols() * this->get_n_rows(); i++) {
+                data[i] = _T{};
+            }
+        };
+
+        void print() const override;
+
+       private:
+        std::shared_ptr<_T[]> data;
+    };
+
+    template <typename _T>
+    class MatrixCRS : public MatrixBase<_T> {
+       public:
+        MatrixCRS();
+        MatrixCRS(size_t n);
+        MatrixCRS(size_t rows, size_t cols);
+        // ~MatrixCRS();
+
+        void construct(size_t rows, size_t cols);
+
+        _T get(size_t i, size_t j) const override;
+        void set(size_t i, size_t j, _T val) override;
+
+        // Proxy class for handling element access and modification
+        class Proxy {
+           public:
+            Proxy(MatrixCRS<_T>& matrix, size_t row, size_t col)
+                : matrix(matrix), row(row), col(col) {}
+
+            operator _T() const {
+                return matrix._unsave_get(row, col);
+            }
+
+            Proxy& operator=(_T val) {
+                matrix._unsave_set(row, col, val);
+                return *this;
+            }
+
+            Proxy& operator+=(_T val) {
+                size_t index = matrix._find(row, col);
+                double entry_val = index != -1 ? matrix.values[index] : 0.0;
+
+                matrix._unsave_set(row, col, index, entry_val + val);
+                return *this;
+            }
+
+            Proxy& operator-=(_T val) {
+                size_t index = matrix._find(row, col);
+                double entry_val = index != -1 ? matrix.values[index] : 0.0;
+
+                matrix._unsave_set(row, col, index, entry_val - val);
+                return *this;
+            }
+
+           private:
+            MatrixCRS<_T>& matrix;
+            size_t row, col;
+        };
+
+        Proxy operator()(size_t row, size_t col) {
+            this->validate(row, col);
+
+            return Proxy(*this, row, col);
+        }
+
+        const _T operator()(size_t row, size_t col) const {
+            this->validate(row, col);
+
+            return _unsave_get(row, col);
+        }
+
+        template <typename _T2>
+        MatrixCRS<_T>& operator+=(MatrixCRS<_T2> o) {
+            if (this->get_n_rows() != o.get_n_rows() || this->get_n_cols() != o.get_n_cols()) {
+                throw std::invalid_argument("Matrix dimensions do not match");
+            }
+
+            size_t r = 0;
+            for (int i = 0; i < o.row_ptrs[this->get_n_rows()]; i++) {
+                if (i == o.row_ptrs[r + 1]) {
+                    r++;
+                }
+                
+                size_t c = o.col_indices[i];
+                
+                size_t index = this->_find(r, c);
+
+                _T o_val = static_cast<_T>(o.values[i]);
+                _T val = index != -1 ? this->values[index] : 0.0;
+                this->_unsave_set(r, c, index, val+o_val);
+            }
+            return *this;
+        }
+
+        void clear() {
+            values.clear();
+            col_indices.clear();
+            for (size_t i = 0; i <= this->rows; i++) {
+                row_ptrs[i] = 0;
+            }
+        }
+
+        static MatrixCRS<_T> from_dense(const Matrix<_T>& mat);
+
+        void print() const override;
+
+       private:
+        std::vector<_T> values;
+        std::vector<size_t> col_indices;
+        std::shared_ptr<size_t[]> row_ptrs;
+
+        void _remove(size_t i, size_t j);
+        void _remove(size_t i, size_t j, size_t index);
+        size_t _find(size_t i, size_t j) const;
+
+        _T _unsave_get(size_t i, size_t j) const;
+        void _unsave_set(size_t i, size_t j, _T val);
+        void _unsave_set(size_t i, size_t j, size_t index, _T val);
+    };
+
+
+    template <typename _T>
+    class MatrixDiagonal : public MatrixBase<_T> {
+       public:
+        MatrixDiagonal(size_t n);
+        MatrixDiagonal(size_t n, _T val);
+        MatrixDiagonal(size_t n, std::vector<_T> vals);
+
+
+        _T get(size_t i, size_t j) const override;
+        void set(size_t i, size_t j, _T val) override;
+
+        void print() const override;
+
+       private:
+        std::shared_ptr<_T[]>  data;
+
+        void diagonal_validate(size_t i, size_t j) const;
+    };
 }  // namespace fin_diff
 
 #include "matrix.tpp"
 
-#endif 
+#endif
--- src/matrix.tpp
+++ src/matrix.tpp
@@ -1,5 +1,6 @@
 #include "matrix.hpp"
 
+#include <cstdlib>
 #include <cstddef>
 #include <stdlib.h>
 #include <vector>
@@ -18,27 +19,62 @@ void Matrix<_T>::print() const {
 }
 
 template <typename _T>
+Matrix<_T>::Matrix() : MatrixBase<_T>(0, 0) {
+    std::cout << "Null Matrix Created" << std::endl;
+}
+
+template <typename _T>
 Matrix<_T>::Matrix(size_t rows, size_t cols) : MatrixBase<_T>(rows, cols) {
-    std::cout << rows << "x" << cols << " Matrix Created" << std::endl;
+    data = std::shared_ptr<_T[]>(new _T[rows * cols]);
+    for (size_t i = 0; i < rows * cols; i++) {
+        data[i] = _T{};
+    }
     
-    data = static_cast<_T*>(std::calloc(rows * cols, sizeof(_T)));
+    std::cout << rows << "x" << cols << " Matrix Created" << std::endl;
 }
 
 template <typename _T>
-Matrix<_T>::~Matrix() {
-    delete[] data;
+Matrix<_T>::Matrix(size_t rows, size_t cols, _T val) : MatrixBase<_T>(rows, cols) {
+    data = std::shared_ptr<_T[]>(new _T[rows * cols]);
+    for (size_t i = 0; i < rows * cols; i++) {
+        data[i] = val;
+    }
+
+    std::cout << rows << "x" << cols << " Matrix Created" << std::endl;
 }
 
 template <typename _T>
-size_t MatrixBase<_T>::get_rows() const {
+Matrix<_T>::Matrix(size_t rows, size_t cols, std::vector<_T> vals) : MatrixBase<_T>(rows, cols) {
+    size_t n_vals = vals.size();
+
+    data = std::shared_ptr<_T[]>(new _T[rows * cols]);
+    for (size_t i = 0; i < rows * cols; i++) {
+        if (i < n_vals) {
+            data[i] = vals[i];
+        } else {
+            data[i] = _T{};
+        }
+    }
+
+    std::cout << rows << "x" << cols << " Matrix Created" << std::endl;
+}
+
+
+
+// template <typename _T>
+// Matrix<_T>::~Matrix() {}
+
+template <typename _T>
+size_t MatrixBase<_T>::get_n_rows() const {
     return rows;
 }
 
 template <typename _T>
-size_t MatrixBase<_T>::get_cols() const {
+size_t MatrixBase<_T>::get_n_cols() const {
     return cols;
 }
 
+// legacy get and set methods
 template <typename _T>
 _T Matrix<_T>::get(size_t i, size_t j) const {
     this->validate(i, j);
@@ -53,7 +89,26 @@ void Matrix<_T>::set(size_t i, size_t j, _T val) {
     data[i * this->cols + j] = val;
 }
 
+template <typename _T>
+_T& Matrix<_T>::operator()(size_t row, size_t col) {
+    this->validate(row, col);
+
+    return data[row * this->cols + col];
+}
+
+template <typename _T>
+const _T& Matrix<_T>::operator()(size_t row, size_t col) const {
+    this->validate(row, col);
+
+    return data[row * this->cols + col];
+}
+
 // MatrixCRS class implementation
+template <typename _T>
+MatrixCRS<_T>::MatrixCRS() : MatrixBase<_T>(0, 0) {
+    this->construct(0, 0);
+}
+
 template <typename _T>
 MatrixCRS<_T>::MatrixCRS(size_t n) : MatrixBase<_T>(n, n) {
     this->construct(n, n);
@@ -66,17 +121,12 @@ MatrixCRS<_T>::MatrixCRS(size_t rows, size_t cols) : MatrixBase<_T>(rows, cols) 
 
 template <typename _T>
 void MatrixCRS<_T>::construct(size_t rows, size_t cols) {
-    this->row_ptrs = new size_t[rows + 1];
-    for (size_t i = 0; i <= rows; ++i) {
-        row_ptrs[i] = 0;
+    this->row_ptrs = std::shared_ptr<size_t[]>(new size_t[rows + 1]);
+    for (size_t i = 0; i <= rows; i++) {
+        this->row_ptrs[i] = 0;
     }
 }
 
-
-template <typename _T>
-MatrixCRS<_T>::~MatrixCRS() {
-    free(row_ptrs);
-}
 
 template <typename _T>
 _T MatrixCRS<_T>::get(size_t i, size_t j) const{
@@ -124,13 +174,72 @@ void MatrixCRS<_T>::set(size_t i, size_t j, _T val) {
     }
 }
 
+
+// new get and set methods using Proxy class
+template <typename _T>
+_T MatrixCRS<_T>::_unsave_get(size_t i, size_t j) const{
+    size_t index = this->_find(i, j);
+
+    if (index != -1) {
+        return values[index];
+    }
+
+    return _T{};
+}
+
+template <typename _T>
+void MatrixCRS<_T>::_unsave_set(size_t i, size_t j, _T val) {
+    if (val == _T{}) {
+        this->_remove(i, j);
+        return;
+    }
+
+    // Find the correct position to insert the value
+    size_t cur_ptr = row_ptrs[i];
+    size_t nxt_row_start_index = row_ptrs[i + 1];
+
+    while (cur_ptr < nxt_row_start_index) {
+        if (col_indices[cur_ptr] == j) {
+            values[cur_ptr] = val;
+            return;
+        } else if (col_indices[cur_ptr] > j) {
+            break;
+        }
+        cur_ptr++;
+    }
+
+    // Insert the value at the correct position
+    values.insert(values.begin() + cur_ptr, val);
+    col_indices.insert(col_indices.begin() + cur_ptr, j);
+
+    // Update the row pointers
+    for (size_t k = i + 1; k <= this->rows; k++) {
+        row_ptrs[k]++;
+    }
+}
+
+template <typename _T>
+void MatrixCRS<_T>::_unsave_set(size_t i, size_t j, size_t index, _T val) {
+    if (val == _T{}) {
+        return this->_remove(i, j, index);
+    }
+
+    if (index != -1) {
+        values[index] = val;
+    } else {
+        _unsave_set(i, j, val);
+    }
+}
+
+
+
 template <typename _T>
 MatrixCRS<_T> MatrixCRS<_T>::from_dense(const Matrix<_T>& mat) {
-    MatrixCRS<_T> crs(mat.get_rows(), mat.get_cols());
+    MatrixCRS<_T> crs(mat.get_n_rows(), mat.get_n_cols());
 
-    for (size_t i = 0; i < mat.get_rows(); i++) {
+    for (size_t i = 0; i < mat.get_n_rows(); i++) {
         bool has_non_zero = false;
-        for (size_t j = 0; j < mat.get_cols(); j++) {
+        for (size_t j = 0; j < mat.get_n_cols(); j++) {
             _T val = mat.get(i, j);
             if (val != _T{}) {
                 crs.values.push_back(val);
@@ -148,7 +257,7 @@ MatrixCRS<_T> MatrixCRS<_T>::from_dense(const Matrix<_T>& mat) {
         }
     }
 
-    crs.row_ptrs[crs.get_rows()] = crs.values.size();
+    crs.row_ptrs[crs.get_n_rows()] = crs.values.size();
 
     return crs;
 }
@@ -179,6 +288,20 @@ void MatrixCRS<_T>::_remove(size_t i, size_t j) {
 }
 
 template <typename _T>
+void MatrixCRS<_T>::_remove(size_t i, size_t j, size_t index) {
+    if (index == -1) {
+        return this->_remove(i, j);
+    }
+
+    values.erase(values.begin() + index);
+    col_indices.erase(col_indices.begin() + index);
+
+    for (size_t k = i + 1; k <= this->rows; k++) {
+        row_ptrs[k]--;
+    }
+}
+
+template <typename _T>
 size_t MatrixCRS<_T>::_find(size_t i, size_t j) const {
     this->validate(i, j);
 
@@ -194,7 +317,71 @@ size_t MatrixCRS<_T>::_find(size_t i, size_t j) const {
         cur_ptr++;
     }
 
-    return -1;
+    return static_cast<size_t>(-1);
 }
+
+// Diagonal Matrix class implementation
+template <typename _T>
+MatrixDiagonal<_T>::MatrixDiagonal(size_t n) : MatrixBase<_T>(n, n) {
+    data = std::shared_ptr<_T[]>(new _T[n]);
+    for (size_t i = 0; i < n; i++) {
+        data[i] = _T{};
+    }
+    
+    std::cout << n << "x" << n << " Diagonal Matrix Created" << std::endl;
+}
+
+template <typename _T>
+MatrixDiagonal<_T>::MatrixDiagonal(size_t n, _T val) : MatrixBase<_T>(n, n) {
+    data = std::shared_ptr<_T[]>(new _T[n]);
+    for (size_t i = 0; i < n; i++) {
+        data[i] = val;
+    }
+
+    std::cout << n << "x" << n << " Diagonal Matrix Created" << std::endl;
+}
+
+template <typename _T>
+MatrixDiagonal<_T>::MatrixDiagonal(size_t n, std::vector<_T> vals) : MatrixBase<_T>(n, n) {
+    size_t n_vals = vals.size();
+
+    data = std::shared_ptr<_T[]>(new _T[n]);
+    for (size_t i = 0; i < n; i++) {
+        if (i < n_vals) {
+            data[i] = vals[i];
+        } else {
+            data[i] = _T{};
+        }
+    }
+
+    std::cout << n << "x" << n << " Diagonal Matrix Created" << std::endl;
+}
+
+template <typename _T>
+_T MatrixDiagonal<_T>::get(size_t i, size_t j) const {
+    this->validate(i, j);
+
+    if (i == j) {
+        return data[i];
+    }
+
+    return _T{};
+}
+
+template <typename _T>
+void MatrixDiagonal<_T>::set(size_t i, size_t j, _T val) {
+    this->validate(i, j);
+    this->diagonal_validate(i, j);
+
+    data[i] = val;
+}
+
+template <typename _T>
+void MatrixDiagonal<_T>::diagonal_validate(size_t i, size_t j) const {
+    if (i != j) {
+        throw std::invalid_argument("Matrix is a diagonal matrix");
+    }
+}
+
 
 }  // namespace fin_diff
